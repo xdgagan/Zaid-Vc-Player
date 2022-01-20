d352fb2614ae43003e475a8bce739c4d564814d9
xdgagan <97921203+xdgagan@users.noreply.github.com>
2022-01-20 14:55:50 +0530

    Update __main__.py

--- Music/__main__.py
+++ Music/__main__.py
@@ -52,7 +52,7 @@ async def load_start():
             print("Error came while clearing db")
             pass     
     await app.send_message(LOG_GROUP_ID, "Music Bot Started")
-    await client.send_message(LOG_GROUP_ID, "Assistant Of Zaid Music Started")
+    await client.send_message(LOG_GROUP_ID, "Assistant Of Asukaa Music Started")
     print("[INFO]: STARTED THE ASUKA BOT AND SENDING THE INFO TO ASUKA SERVER")
     
    
